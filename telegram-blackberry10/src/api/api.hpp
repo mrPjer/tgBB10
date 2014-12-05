f76452ffcd630d0aa8b32feef706de43a39e0d34
ChubbyHunteR <Alfis@Mekansm>
2014-12-05 18:37:58 +0100

    Created api.hpp

---
+++ telegram-blackberry10/src/api/api.hpp
@@ -0,0 +1,20 @@
+/*
+ * api.hpp
+ *
+ *  Created on: 5. 12. 2014.
+ *      Author: Alfis
+ */
+
+#ifndef API_HPP_
+#define API_HPP_
+
+class API{
+private:
+
+protected:
+
+};
+
+
+
+#endif /* API_HPP_ */
