a7d79ab309352332cbbccea24d1c5520f40350c1
ChubbyHunteR <luka.strizic@hotmail.com>
2014-12-05 20:26:48 +0100

    Made api.run a pure virtual function, making api an abstract function

---
+++ telegram-blackberry10/src/api/api.cpp
@@ -0,0 +1,25 @@
+/*
+ * api.cpp
+ *
+ *  Created on: 5. 12. 2014.
+ *      Author: Alfis
+ */
+
+#include <api.hpp>
+
+unsigned long API::sleepFrom = 300;
+unsigned long API::sleepTo = 700;
+
+API::API(){
+    QTime time = QTime::currentTime();
+    qsrand(time.msec());
+}
+
+void API::setSleepFrom(unsigned long msec){
+    sleepFrom = msec;
+}
+
+void API::setSleepTo(unsigned long msec){
+    sleepTo = msec;
+}
+
--- telegram-blackberry10/src/api/api.hpp
+++ telegram-blackberry10/src/api/api.hpp
@@ -8,13 +8,32 @@
 #ifndef API_HPP_
 #define API_HPP_
 
-class API{
+#include <QThread>
+#include <QTime>
+#include <QGlobal.h>
+#include <unistd.h>
+using namespace std;
+
+class API: public QThread{
+    Q_OBJECT
 private:
+    static unsigned long sleepFrom;
+    static unsigned long sleepTo;
+    virtual void run() = 0;
 
 protected:
+    void delay(){
+        ::delay(qrand() % (sleepTo - sleepFrom + 1) + sleepFrom);
+    }
+
+public:
+    API();
+
+public slots:
+    void setSleepFrom(unsigned long msec);
+    void setSleepTo(unsigned long msec);
 
 };
-
 
 
 #endif /* API_HPP_ */
