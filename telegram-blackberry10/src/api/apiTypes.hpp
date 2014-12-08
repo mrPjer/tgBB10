e140a41a8169fea0d7b5d5e6c3c35418dabc064a
ChubbyHunteR <Alfis@Mekansm>
2014-12-08 16:35:06 +0100

    Mock API account.anything is done.

--- telegram-blackberry10/src/api/apiTypes.hpp
+++ telegram-blackberry10/src/api/apiTypes.hpp
@@ -9,6 +9,8 @@
 #define APITYPES_HPP_
 
 #include <QString>
+#include <vector>
+using namespace std;
 
 struct PhoneChecked: public QObject{
     Q_OBJECT
@@ -100,6 +102,8 @@ public:
     int id;
     QString first_name;
     QString last_name;
+    QString username;
+    long access_hash;
     QString phone;
     UserProfilePhoto* photo;
     UserStatus* status;
@@ -128,6 +132,127 @@ public:
 
     int id;
     QString bytes;
+};
+
+struct InputPeer: public QObject{
+    Q_OBJECT
+    Q_PROPERTY(int user_id READ getUserID)
+    Q_PROPERTY(long access_hash READ getAccessHash)
+    Q_PROPERTY(int chat_id READ getChatID)
+public:
+    int getUserID(){return user_id;}
+    long getAccessHash(){return access_hash;}
+    int getChatID(){return chat_id;}
+
+    int user_id;
+    long access_hash;
+    int chat_id;
+};
+
+struct InputGeoChat: public QObject{
+    Q_OBJECT
+    Q_PROPERTY(int chat_id READ getCharID)
+    Q_PROPERTY(long access_hash READ getAccessHash)
+public:
+    int getChatID(){return chat_id;}
+    long getAccessHash(){return access_hash;}
+
+    int chat_id;
+    long access_hash;
+};
+
+struct InputNotifyPeer: public QObject{
+    Q_OBJECT
+    Q_PROPERTY(InputPeer* peer READ getPeer)
+    Q_PROPERTY(InputGeoChat* peer_geo_chat READ getPeerGeoChat)
+public:
+    InputPeer* getPeer(){return peer;}
+    InputGeoChat* getPeerGeoChat(){return peer_geo_chat;}
+
+    InputPeer* peer;
+    InputGeoChat* peer_geo_chat;
+};
+
+struct InputPeerNotifyEvents: public QObject{
+    Q_OBJECT
+    Q_PROPERTY(bool all READ isAll)
+public:
+    bool isAll(){return all;}
+
+    bool all;
+};
+
+struct PeerNotifySettings: public QObject{
+    Q_OBJECT
+    Q_PROPERTY(int mute_until READ getMuteUntil)
+    Q_PROPERTY(QString sound READ getSound)
+    Q_PROPERTY(bool show_previews READ isShowingPreviews)
+    Q_PROPERTY(int events_mask READ getEventsMask)
+public:
+    int getMuteUntil(){return mute_until;}
+    QString getSound(){return sound;}
+    bool isShowingPreviews(){return show_previews;}
+    int getEventsMask(){return events_mask;}
+
+    int mute_until;
+    QString sound;
+    bool show_previews;
+    int events_mask;
+
+};
+
+struct InputPeerNotifySettings: public PeerNotifySettings{
+    Q_OBJECT
+    Q_PROPERTY(InputPeerNotifyEvents* events READ getEvents)
+public:
+    InputPeerNotifyEvents* getEvents(){return events;}
+
+    InputPeerNotifyEvents* events;
+};
+
+struct PhotoSize{
+    Q_OBJECT
+    Q_PROPERTY(QString type READ getType)
+    Q_PROPERTY(FileLocation* location READ getLocation)
+    Q_PROPERTY(int w READ getW)
+    Q_PROPERTY(int h READ getH)
+    Q_PROPERTY(int size READ getSize)
+    Q_PROPERTY(QString bytes READ getBytes)
+public:
+    QString getType(){return type;}
+    FileLocation* getLocation(){return location;}
+    int getW(){return w;}
+    int getH(){return h;}
+    int getSize(){return size;}
+    QString getBytes(){return bytes;}
+
+    QString type;
+    FileLocation* location;
+    int w;
+    int h;
+    int size;
+    QString bytes;
+};
+
+struct WallPaper{
+    Q_OBJECT
+    Q_PROPERTY(int id READ getID)
+    Q_PROPERTY(QString title READ getTitle)
+    Q_PROPERTY(vector<PhotoSize>* READ getSizes)
+    Q_PROPERTY(int color READ getColor)
+    Q_PROPERTY(int bg_color READ getBgColor)
+public:
+    int getID(){return id;}
+    QString getTitle(){return title;}
+    vector<PhotoSize>* getSizes(){return sizes;}
+    int getColor(){return color;}
+    int getBgColor(){return bg_color;}
+
+    int id;
+    QString title;
+    vector<PhotoSize>* sizes;
+    int color;
+    int bg_color;
 };
 
 
