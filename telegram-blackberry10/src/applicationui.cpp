b1b4d62364d6f0342ca5e04cbaa39f5704cd2e4d
Petar Šegina <psegina@ymail.com>
2014-12-13 01:56:41 +0100

    Register tgApi as registration API

--- telegram-blackberry10/src/applicationui.cpp
+++ telegram-blackberry10/src/applicationui.cpp
@@ -24,7 +24,15 @@
 #include "util/timer.hpp"
 #include "util/countries.hpp"
 
+#include "config.hpp"
+
+#ifdef TG_API_MOCK
 #include "api/apiRegAuth.hpp"
+#endif
+
+#ifdef TG_API_TG
+#include "api/tgApi.hpp"
+#endif
 
 using namespace bb::cascades;
 
@@ -34,8 +42,13 @@ ApplicationUI::ApplicationUI() :
     // Register our Timer class in QML
     qmlRegisterType<Timer>("Timer", 1, 0, "Timer");
 
+#ifdef TG_API_MOCK
     // Register out Registration API in QML
     qmlRegisterType<APIRegAuth>("TgApi", 1, 0, "RegistrationApi");
+#endif
+#ifdef TG_API_TG
+    qmlRegisterType<tgApi>("TgApi", 1, 0, "RegistrationApi");
+#endif
 
     // prepare the localization
     m_pTranslator = new QTranslator(this);
