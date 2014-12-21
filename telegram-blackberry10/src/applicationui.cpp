13f42536764569bde03b434134afc37c56bb60cf
Petar Šegina <psegina@ymail.com>
2014-12-22 00:03:16 +0100

    Open appropriate page depending on environment
    If we're in the mock environment, show the debug main.qml which contains
    links to other UI components.
    
    If we're in the tg envirnoment, depending on the existance of an app
    secret in the session store, show either the intro screen or the main
    screen.

--- telegram-blackberry10/src/applicationui.cpp
+++ telegram-blackberry10/src/applicationui.cpp
@@ -71,9 +71,31 @@ ApplicationUI::ApplicationUI() :
     // initial load
     onSystemLanguageChanged();
 
+    QString pageAsset;
+
+#ifdef TG_API_TG
+    TgSession session;
+
+    if(session.isSessionStored()) {
+        QByteArray secret = session.session();
+        qDebug() << "Existing session already present - " << secret;
+
+        tgApi api;
+        api.restoreConnection(secret);
+        pageAsset = "asset:///main/MainPage.qml";
+    } else {
+        qDebug() << "No existing session";
+        pageAsset = "asset:///intro/IntroWithPane.qml";
+    }
+#endif
+#ifdef TG_API_MOCK
+    pageAsset = "asset:///main.qml";
+#endif
+
+
     // Create scene document from main.qml asset, the parent is set
     // to ensure the document gets destroyed properly at shut down.
-    QmlDocument *qml = QmlDocument::create("asset:///main.qml").parent(this);
+    QmlDocument *qml = QmlDocument::create(pageAsset).parent(this);
 
     // Create root object for the UI
     AbstractPane *root = qml->createRootObject<AbstractPane>();
